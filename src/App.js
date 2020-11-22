1e07c404045f594749f638a638df2733ac0c2084
Manjiri 1101 <Manjiri1101@users.noreply.github.com>
2020-11-21 21:54:41 -0800

    Revert "added email"
    This reverts commit 067189f0a16987042dfb563d649dd502be433d6e.

--- src/App.js
+++ src/App.js
@@ -5,34 +5,24 @@ import { Interactions } from 'aws-amplify';
 import { AmplifyChatbot } from "@aws-amplify/ui-react";
 import config from './aws-exports';
 import Amplify from 'aws-amplify';
-
-//import { AmplifyAuthenticator, AmplifySignOut, AmplifySignIn, AmplifySignUp } from '@aws-amplify/ui-react'
-
 Amplify.configure(config);
 
 function App() {
-  const [userEmail, setUserEmail] = useState("");
-  async function getEmail() {
-		console.log("Getting email...");
-		Auth.currentUserInfo()
-			.then((res) => {
-				setUserEmail(res.attributes.email);
-
-				console.log("Email is: " + res.attributes.email + "!");
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-  }
-  useEffect(() => {
-		
-		getEmail();
-	}, []);
   return (
     <div className="App">
       <header className="App-header">
-        <h>{userEmail}</h>
-     
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
         <AmplifyChatbot
           botName="calculatecarbonemissionbot_dev"
           botTitle="Calculate carbon emission bot"
