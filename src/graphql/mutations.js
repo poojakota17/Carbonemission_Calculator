7082586c0bb5ed0f6dbf67d5c72e63a304d4e61d
Manjiri 1101 <Manjiri1101@users.noreply.github.com>
2020-11-22 13:47:29 -0800

    Added dynamodb for user and budget

---
+++ src/graphql/mutations.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModeluserConditionInput
+  ) {
+    createUser(input: $input, condition: $condition) {
+      id
+      username
+      budget
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModeluserConditionInput
+  ) {
+    updateUser(input: $input, condition: $condition) {
+      id
+      username
+      budget
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModeluserConditionInput
+  ) {
+    deleteUser(input: $input, condition: $condition) {
+      id
+      username
+      budget
+      createdAt
+      updatedAt
+    }
+  }
+`;
