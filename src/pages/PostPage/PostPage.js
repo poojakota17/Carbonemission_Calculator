89f2dba7ab16f419a70af7410bb71a4a5f6ecf3c
Manjiri 1101 <Manjiri1101@users.noreply.github.com>
2020-11-27 13:01:21 -0800

    Added drawn imgs, post pg, stat pg done

--- src/pages/PostPage/PostPage.js
+++ src/pages/PostPage/PostPage.js
@@ -1,142 +1,136 @@
 
 import React, { useState, useEffect, useReducer } from 'react';
-import {WNavBar} from '../../components/WNavBar'
+import { WNavBar } from '../../components/WNavBar'
 import { API, Auth } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listPosts } from '../../graphql/queries';
 import { createPost as createPostMutation, deletePost as deletePostMutation, updatePost as updatePostMutation } from '../../graphql/mutations';
-import { Container ,Form, Button, Row, Col} from 'react-bootstrap';
-import {BiBulb} from "react-icons/bi";
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+import { BiBulb } from "react-icons/bi";
 import { IoMdTrash, IoIosHeart } from "react-icons/io";
 import './PostPage.css';
-const initialFormState = { title: '', post : '', uname: '', like: 0}
+const initialFormState = { title: '', post: '', uname: '', like: 0 }
 const PostPage = (props) => {
-  const [posts, setPosts] = useState([]);
-  const [userEmail, setUserEmail] = useState("");
-  const [formData, setFormData] = useState(initialFormState);
+    const [posts, setPosts] = useState([]);
+    const [userEmail, setUserEmail] = useState("");
+    const [formData, setFormData] = useState(initialFormState);
 
-  useEffect(() => {
-    fetchPosts();
-    getEmail();
-  }, []);
-  async function getEmail(){
-    console.log("Getting email...");
-    Auth.currentUserInfo()
-      .then((res) => {
-        setUserEmail(res.attributes.email);
-       console.log("Email is: " + res.attributes.email);
-       console.log("email is :", userEmail);
-       setFormData({...formData, uname: res.attributes.email, like:0});
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    useEffect(() => {
+        fetchPosts();
+        getEmail();
+    }, []);
+    async function getEmail() {
+        console.log("Getting email...");
+        Auth.currentUserInfo()
+            .then((res) => {
+                setUserEmail(res.attributes.email);
+                console.log("Email is: " + res.attributes.email);
+                console.log("email is :", userEmail);
+                setFormData({ ...formData, uname: res.attributes.email, like: 0 });
+            })
+            .catch((err) => {
+                console.error(err);
+            });
 
-  }
-
-  async function fetchPosts() {
-    const apiData = await API.graphql({ query: listPosts });
-    setPosts(apiData.data.listPosts.items);
-  }
-  async function createPost() {
-    if (!formData.title || !formData.post) return;
-    await API.graphql({ query: createPostMutation, variables: { input: formData } });
-    setPosts([ ...posts, formData ]);
-    setFormData(initialFormState);
-  }
-  async function deletePost({ id }) {
-    const newPostsArray = posts.filter(post => post.id !== id);
-    setPosts(newPostsArray);
-    await API.graphql({ query: deletePostMutation, variables: { input: { id } }});
-  }
-
-  async function hitLike(idx){
-
-    const post=posts[idx];
-    console.log("const post",post);
-    post.like=post.like+1;
-    delete post.createdAt;
-    delete post.updatedAt;
-    delete post.owner;
-    try{
-        const postData = await API.graphql({query: updatePostMutation, variables: { input: post } });
-        console.log("postData", postData);
-    }catch(err) {
-        console.log("OOPS!");
-        console.log(err);
     }
 
-  }
+    async function fetchPosts() {
+        const apiData = await API.graphql({ query: listPosts });
+        setPosts(apiData.data.listPosts.items);
+    }
+    async function createPost() {
+        if (!formData.title || !formData.post) return;
+        await API.graphql({ query: createPostMutation, variables: { input: formData } });
+        setPosts([...posts, formData]);
+        setFormData(initialFormState);
+    }
+    async function deletePost({ id }) {
+        const newPostsArray = posts.filter(post => post.id !== id);
+        setPosts(newPostsArray);
+        await API.graphql({ query: deletePostMutation, variables: { input: { id } } });
+    }
 
+    async function hitLike(idx) {
 
+        const post = posts[idx];
+        console.log("const post", post);
+        post.like = post.like + 1;
+        delete post.createdAt;
+        delete post.updatedAt;
+        delete post.owner;
+        try {
+            const postData = await API.graphql({ query: updatePostMutation, variables: { input: post } });
+            console.log("postData", postData);
+        } catch (err) {
+            console.log("OOPS!");
+            console.log(err);
+        }
 
+    }
     return (
         <>
             <WNavBar />
             <center><h3>Share the Steps you are taking to make the world a better place !</h3></center>
-            <br/>
+            <br />
             <Container>
-                
-                    <Form>
-                        <Row>
-                            <Col>    
-                                <Form.Control type="text" onChange={e => setFormData({ ...formData, 'title': e.target.value })}
-                                    placeholder="Enter Title"
-                                    value={formData.title} />
-                                    <br />
 
-                                <Form.Control type="text"
-                                    onChange={e => setFormData({ ...formData, 'post': e.target.value })}
-                                    placeholder="Share the thoughts"
-                                    value={formData.post} />
-                            </Col>    
-                            <Col xs lg="2">
-                                <Button variant="primary"  onClick={createPost}>
-                                    Post
+                <Form>
+                    <Row>
+                        <Col>
+                            <Form.Control type="text" onChange={e => setFormData({ ...formData, 'title': e.target.value })}
+                                placeholder="Enter Title"
+                                value={formData.title} />
+                            <br />
+                            <Form.Control type="text"
+                                onChange={e => setFormData({ ...formData, 'post': e.target.value })}
+                                placeholder="Share the thoughts"
+                                value={formData.post} />
+                        </Col>
+                        <Col xs lg="2">
+                            <Button variant="primary" onClick={createPost}>
+                                Post
                                 </Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                
-            </Container>
-            <br/>
-            <hr/>
-            <Container>
-               
-                
-            <div className="container">
-                {posts.map((post, idx) => {
-                    return (
-                        <div>
-                            <div key={`post${idx}`}>
-                                <fieldset  className='mycard'>
-                                    <legend className='postTitle'><h2>{post.title}</h2></legend>
-                                    <div>
-                                        <div className='row'>
-                                            <div key={post.id || post.title} className='col-lg-6'>
-                                                
-                                                <p>{post.post}</p>
+                        </Col>
+                    </Row>
+                </Form>
 
-                                                <p>Posted by : {post.uname}</p>
-                                                <p>{post.createdAt}</p>
-                                            </div>
-                                            <div className='col-lg-6'>    
-                                                <Button className="btn btn-warning likebutton" onClick={() => hitLike(idx)}>
-                                                    <p>{post.like}</p>
-                                                    <IoIosHeart />
-                                                </Button>
+            </Container>
+            <br />
+            <hr />
+            <Container>
+                <div className="container">
+                    {posts.map((post, idx) => {
+                        return (
+                            <div>
+                                <div key={`post${idx}`}>
+                                    <fieldset className='mycard'>
+                                        <legend className='postTitle'><h2>{post.title}</h2></legend>
+                                        <div>
+                                            <div className='row'>
+                                                <div key={post.id || post.title} className='col-lg-6'>
+
+                                                    <p>{post.post}</p>
+
+                                                    <p>Posted by : {post.uname}</p>
+                                                    <p>{post.createdAt}</p>
+                                                </div>
+                                                <div className='col-lg-6'>
+                                                    <Button className="btn btn-warning likebutton" onClick={() => hitLike(idx)}>
+                                                        <p>{post.like}</p>
+                                                        <IoIosHeart />
+                                                    </Button>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </fieldset>
+                                    </fieldset>
+                                </div>
+                                <br />
                             </div>
-                            <br/>
-                        </div>
-                    );
-                })}
-            </div>
-            <br/>
-            <br/>
+                        );
+                    })}
+                </div>
+                <br />
+                <br />
             </Container>
 
         </>
@@ -144,43 +138,3 @@ const PostPage = (props) => {
 };
 export default PostPage;
 
-/*
-<div style={{marginBottom: 30}}>
-        {
-          posts.map(post => (
-            <div key={post.id || post.uname}>
-              <h2>{post.title}</h2>
-              <p>{post.post}</p>
-              <button onClick={() => deletePost(post)}>Delete post</button>
-            </div>
-          ))
-        }
-      </div>
-
-        <input type="text"
-                  onChange={e => setFormData({ ...formData, 'title': e.target.value })}
-                  placeholder="Title"
-                  value={formData.title}
-              />
-               <input type="text"
-                  onChange={e => setFormData({ ...formData, 'post': e.target.value })}
-                  placeholder="Share the thoughts"
-                  value={formData.post}
-              />
-
-              <div style={{marginBottom: 30}}>
-        {
-          posts.map(post => (
-            <div key={post.id || post.uname}>
-              <h2>{post.title}</h2>
-              <p>{post.post}</p>
-              <button onClick={() => deletePost(post)}>Delete post</button>
-            </div>
-          ))
-        }
-      </div>
-
-      <Button className="btn btn-warning" onClick={() => deletePost(post)}>
-                                        <IoMdTrash />
-                                    </Button>
-*/
--- src/pages/StaticPage/StaticPage.js
+++ src/pages/StaticPage/StaticPage.js
@@ -1,55 +1,32 @@
 import React, { Component } from 'react';
-//import { StaticPage } from '.';
 import './StaticPage.css';
 import {
     Link
 } from 'react-router-dom';
+import { TwitterTimelineEmbed, TwitterHashtagButton, TwitterTweetEmbed } from 'react-twitter-embed';
 //import { ReactComponent as Logo} from '../../logo.svg';
 import { WNavBar } from '../../components/WNavBar'
-//import Twit from 'twit';
-//import dotenv from dotenv;
 import { Card, Button, ListGroup, ListGroupItem, Container, Row, Col, rounded } from 'react-bootstrap';
-
-
-/*var client = new Twit({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  }); */
-
 const StaticPage = props => {
-   // T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
-     //   console.log(data)
-     // })
-
     return (
         <div>
             <WNavBar />
-        
             <h3 className='caption'>Little Steps towards better world !</h3>
             <Link to="/static">StaticPage</Link>
             <div className="class1"></div>
             <br />
-            
             <Container>
                 <Row>
                     <Col>
                         <Card className='myCard'>
                             <Card.Body>
-                                <Card.Title>Renweable Energy</Card.Title>
-                                <Card.Img variant="top" src="/images/renewable.jpg" rounded />
+                                <Card.Title>Renewable Energy</Card.Title>
+                                <Card.Img variant="top" src="/images/renewable1.jpg" rounded />
                                 <Card.Text>
-                                    Wind Energy, Hydro Energy, these are renewable energies
+                                    Using Renewable energies such as wind, hydro power helps reducing carbon footprint as it produce "No Greenhouse Gases".
                                 </Card.Text>
                             </Card.Body>
-                            <ListGroup className="list-group-flush">
-                                <ListGroupItem>You save CO2e </ListGroupItem>
-
-                            </ListGroup>
                             <Card.Body>
-                            <a href="https://twitter.com/intent/tweet?button_hashtag=RenewableEnergy&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #RenewableEnergy</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                            
                                 <Card.Link href="https://www.epa.gov/statelocalenergy/local-renewable-energy-benefits-and-resources#:~:text=Environmental%20and%20economic%20benefits%20of,reducing%20dependence%20on%20imported%20fuels">Read More</Card.Link>
                             </Card.Body>
                         </Card>
@@ -57,17 +34,13 @@ const StaticPage = props => {
                     <Col>
                         <Card className='myCard'>
                             <Card.Body>
-                                <Card.Title>Solar Energy</Card.Title>
-                                <Card.Img variant="top" src="/images/solar.jpg" rounded />
+                                <Card.Title>Solar Electricity </Card.Title>
+                                <Card.Img variant="top" src="/images/solar1.jpg" rounded />
                                 <Card.Text>
-                                    Use Solar energy, its free and readily available. Install solar panels.
-                                 </Card.Text>
+                                    Use Solar energy, its free and readily available. Install solar panels, cook food on Solar cooker!
+                         </Card.Text>
                             </Card.Body>
-                            <ListGroup className="list-group-flush">
-                                <ListGroupItem>Use solar Energy</ListGroupItem>
-                            </ListGroup>
                             <Card.Body>
-                                
                                 <Card.Link href="https://www.seia.org/initiatives/about-solar-energy#:~:text=Solar%20power%20is%20energy%20from,solar%20resources%20in%20the%20world.">Read More</Card.Link>
                             </Card.Body>
                         </Card>
@@ -75,43 +48,31 @@ const StaticPage = props => {
                     <Col>
                         <Card className='myCard'>
                             <Card.Body>
-                                <Card.Title>Go Vegan</Card.Title>
-                                <Card.Img variant="top" src="/images/vegan.jpg" rounded />
+                                <Card.Title>Go Vegan !</Card.Title>
+                                <Card.Img variant="top" src="/images/vegan1.jpg" rounded />
                                 <Card.Text>
-                                    Go vegan! Reduce meat comsumption !
+                                    Eat more vegies, good for health and for environment. We can save Earth by avoifing eating meat, as it has big Carbon Footprint!
                                  </Card.Text>
                             </Card.Body>
-                            <ListGroup className="list-group-flush">
-                                <ListGroupItem>You save CO2e 13499 Kg/ Year</ListGroupItem>
-
-                            </ListGroup>
                             <Card.Body>
-
                                 <Card.Link href="https://www.vegansociety.com/go-vegan/definition-veganism">Read More</Card.Link>
                             </Card.Body>
                         </Card>
                     </Col>
                 </Row>
             </Container>
-            <br/>
-            <br/>
+            <br />
+            <br />
             <Container>
                 <Row>
                     <Col>
                         <Card className='myCard'>
                             <Card.Body>
                                 <Card.Title>Recycle</Card.Title>
-                                <Card.Img variant="top" src="/images/recycle.jpg" rounded />
+                                <Card.Img variant="top" src="/images/recycle1.jpg" rounded />
                                 <Card.Text>
-                                    1
+                                    We produce a lot of waste each year, we can reduce carbon emission by recycling things such as plastic, paper, cans, etc.
                                 </Card.Text>
-                            </Card.Body>
-                            <ListGroup className="list-group-flush">
-                                <ListGroupItem>You save CO2e 13499 Kg/ Year</ListGroupItem>
-
-                            </ListGroup>
-                            <Card.Body>
-
                                 <Card.Link href="https://www.epa.gov/recycle/recycling-basics">Read More</Card.Link>
                             </Card.Body>
                         </Card>
@@ -119,18 +80,13 @@ const StaticPage = props => {
                     <Col>
                         <Card className='myCard'>
                             <Card.Body>
-                                <Card.Title>Reuse</Card.Title>
-                                <Card.Img variant="top" src="/images/reuse.jpg" rounded />
+                                <Card.Title>Reuse !!</Card.Title>
+                                <Card.Img variant="top" src="/images/reuse1.jpg" rounded />
                                 <Card.Text>
-                                    2
+                                    Reuse bags, boxes, things which you think of throwing. Stop. Think. Be creative, reuse them to save Earth.
                                 </Card.Text>
                             </Card.Body>
-                            <ListGroup className="list-group-flush">
-                                <ListGroupItem>You Save :</ListGroupItem>
-
-                            </ListGroup>
                             <Card.Body>
-
                                 <Card.Link href="https://plastic.education/how-do-reusable-bags-help-the-environment/">Read More</Card.Link>
                             </Card.Body>
                         </Card>
@@ -140,37 +96,40 @@ const StaticPage = props => {
                             <Card.Body>
                                 <Card.Title>Reduce food waste
                                 </Card.Title>
-                                <Card.Img variant="top" src="/images/waste.jpg" rounded />
+                                <Card.Img variant="top" src="/images/waste1.jfif" className="img" />
                                 <Card.Text>
-                                    Food waste produces most of the green house gases, please avoid food waste
+                                    Food waste, decayed food produces lots of Methane, and other harmful Greenhouse gases. So try to avoid food wastage. Make compost! Give back to mother nature.
                                 </Card.Text>
                             </Card.Body>
-                            <ListGroup className="list-group-flush">
-                                <ListGroupItem>You save CO2e:    / Year</ListGroupItem>
-
-                            </ListGroup>
                             <Card.Body>
-
                                 <Card.Link href="https://www.worldwildlife.org/stories/fight-climate-change-by-preventing-food-waste#:~:text=But%20wasted%20food%20isn't,more%20potent%20than%20carbon%20dioxide.">Read More</Card.Link>
                             </Card.Body>
                         </Card>
                     </Col>
                 </Row>
             </Container>
-        <br/>  
-        <div> 
-       
-        </div> 
-
-<br/>
-<br />
-<a class="twitter-timeline" href="https://twitter.com/gcarbonproject?ref_src=twsrc%5Etfw">Tweets by gcarbonproject</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-<a class="twitter-timeline" href="https://twitter.com/SavePlanetEart4?ref_src=twsrc%5Etfw">Save Earth</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-<div>
-<script src="https://apps.elfsight.com/p/platform.js" defer></script>
-<div class="elfsight-app-0d1cd714-8c4e-493e-9ff4-cf2e9f022f3e"></div>
-</div>
-    </div>
+            <br />
+            <div>
+            </div>
+            <br />
+            <br />
+            <Container className="tweet">
+                <Row>
+                    <TwitterTimelineEmbed className="tweet"
+                        sourceType="profile"
+                        screenName="SavePlanetEart4"
+                        options={{ height: 500, width: 700 }}
+                    />
+                </Row>
+                <Row>
+                    <TwitterTimelineEmbed className="tweet"
+                        sourceType="profile"
+                        screenName="GlobalCarbonProject"
+                        options={{ height: 500, width: 700 }}
+                    />
+                </Row>
+            </Container>
+        </div>
     );
 };
 
